b1dd2a295285ce6f3b42eea91c7d25c3e6675ce3
dikvangenuchten <31375117+dikvangenuchten@users.noreply.github.com>
2020-03-24 23:06:03 +0100

    final version

--- src/main/java/experiments/hyperParameterFinder.java
+++ src/main/java/experiments/hyperParameterFinder.java
@@ -8,9 +8,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class hyperParameterFinder {
+    /**
+     * Tests a few different hyperparameters, used to get the results for the report in a somewhat automated way.
+     *
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
+        // int[] batchSizes = {16, 32, 64, 128}; //Cifar is to big to do batch 128
         int[] batchSizes = {16, 32, 64};
-        double[] learningRates = {0.1, 0.05, 0.01};
+        double[] learningRates = {0.1}; //When using ADADELTA learning rate is not used
+        //double[] learningRates = {0.1, 0.05, 0.01};
         List<BatchResult> batchResults = new ArrayList<>();
         for (double learningRate : learningRates) {
             for (int batchSize : batchSizes) {
